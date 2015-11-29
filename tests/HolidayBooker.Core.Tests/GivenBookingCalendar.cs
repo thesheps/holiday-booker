1166b4bc29688384fa1f6e3b72764af515c84fe5
Chris Shepherd <thesheps@gmail.com>
2015-11-29 19:52:20 +0000

    Initial check-in with failing test to verify AppHarbor is working as expected

---
+++ tests/HolidayBooker.Core.Tests/GivenBookingCalendar.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace HolidayBooker.Core.Tests
+{
+    public class GivenBookingCalendar
+    {
+        [Test]
+        public void WhenIRetrieveAvailableDate_ThenDatesAreRetrieved()
+        {
+            Assert.True(1 == 2);
+        }
+    }
+}
